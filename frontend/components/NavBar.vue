02c259447cb2845002193ef5dff94d64cc222f3b
gowoonhae <gowoonhae@gmail.com>
2023-04-22 12:12:50 +0900

    feat : add useFetch

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,6 @@
+<template>
+    <nav class="fixed w-full bg-slate-300 space-x-4 text-2xl p-4">
+        <span><NuxtLink to="/">메인</NuxtLink></span>
+        <span><NuxtLink to="/shop">상품</NuxtLink></span>
+    </nav>
+</template>
